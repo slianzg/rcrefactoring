7d25a993bb4a79c81812caecce9776e8084fb9c9
slianzg <slian201248@gmail.com>
2024-02-06 12:28:36 +0900

    Acess/refresh token 구현(not working)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import logMiddleware from './middlewares/log.middleware.js';
 import UsersRouter from './routes/users.router.js';
 import ResumesRouter from './routes/resumes.router.js';
+import AuthRouter from './routes/auth.router.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling,middleware.js';
 
 const app = express();
@@ -12,10 +13,11 @@ app.use(logMiddleware);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api',[UsersRouter, ResumesRouter]);
+app.use('/auth', AuthRouter);
+app.use('/api', [UsersRouter, ResumesRouter]);
 
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
-    console.log(PORT, '포트로 서버가 열렸어요!');
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
---
+++ routes/auth.router.js
@@ -0,0 +1,46 @@
+import experss from 'express';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../utils/prisma/index.js';
+import dotenv from 'dotenv';
+
+const router = experss.Router();
+
+dotenv.config();
+
+router.post('/token', async (req, res, next) => {
+  const { refreshtoken } = req.cookies;
+
+  const token = jwt.verify(refreshtoken, process.env.REFRESH_TOKEN_KEY);
+  if (!token.userId) {
+    return res.status(401).end();
+  }
+
+  //refresh토큰이 유효할 시
+  const user = await prisma.users.findFirst({
+    userId: token.userId,
+  });
+  if (!user) {
+    return res.status(401).end();
+  }
+
+  const newAccessToken = jwt.sign(
+    { userId: user.userId },
+    process.env.TOKEN_SECRET_KEY,
+    { expiresIn: '12h' }
+  );
+  const newRefreshToken = jwt.sign(
+    { userId: user.userId },
+    process.env.REFRESH_TOKEN_KEY,
+    { expiresIn: '7d' }
+  );
+
+  res.cookie('authorization', `Bearer ${newAccessToken}`);
+  res.cookie('refreshtoken', newRefreshToken);
+
+  return res.json({
+    accesstoken: newAccessToken,
+    refreshtoken: newRefreshToken,
+  });
+});
+
+export default router;
